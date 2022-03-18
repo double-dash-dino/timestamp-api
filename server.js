5d66018699185d44130d85ce54b364ebcce54342
Lionel De Swarte--Danko (He/him) <64731565+double-dash-dino@users.noreply.github.com>
2022-03-18 10:47:43 +0000

    refactor

--- server.js
+++ server.js
@@ -5,42 +5,25 @@ import moment from "moment";
 const app = express();
 app.use(cors());
 
-const makeDateJsonObject = (date) => {
-  let unixTimestamp = parseInt(date.getTime() * 1000).toFixed(0);
-  let utcTimestamp = date.toString();
-  let response_object = {
-    unix: unixTimestamp,
-    utc: utcTimestamp,
-  };
-  return response_object;
-};
-
-app.get("/", function (req, res) {
-  res.send(moment.utc());
-});
-
 // If no date, return current date
 app.get("/api", function (req, res) {
-  res.send(makeDateJsonObject(new Date())).json();
+  res.send({
+    unix: moment().format("x") * 1000,
+    utc: moment().format("ddd D MMM YYYY HH:MM:SS [GMT]"),
+  });
 });
 
 app.get("/api/:date", function (req, res) {
-  const isDate = (date) => {
-    if (date.length < 7) {
-      return false;
-    } else {
-      return !isNaN(Date.parse(date));
-    }
-  };
-
   //   If date, return correct json object, else return error
-  if (
-    moment(req.params.date, "x", true).isValid() ||
-    moment(req.params.date, "YYYY-MM-DD", true).isValid()
-  ) {
+  if (moment(req.params.date, "x", true).isValid()) {
     res.send({
       unix: req.params.date,
       utc: new Date(parseInt(req.params.date, 10)).toString(),
+    });
+  } else if (moment(req.params.date, "YYYY-MM-DD", true).isValid()) {
+    res.send({
+      unix: moment(req.params.date).format("x") * 1000,
+      utc: moment(req.params.date).format("ddd D MMM YYYY HH:MM:SS [GMT]"),
     });
   } else {
     res.send("error: Invalid Date");
